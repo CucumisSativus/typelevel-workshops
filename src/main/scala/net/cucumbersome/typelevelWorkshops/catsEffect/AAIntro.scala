f8eb780c16bd7162cff333153796e4b81afe5a4b
Michał Gutowski <michal@cucumbersome.net>
2018-12-15 17:27:21 +0100

    [cats-effect] first lecture part finished

--- src/main/scala/net/cucumbersome/typelevelWorkshops/catsEffect/AAIntro.scala
+++ src/main/scala/net/cucumbersome/typelevelWorkshops/catsEffect/AAIntro.scala
@@ -1,4 +1,5 @@
 package net.cucumbersome.typelevelWorkshops.catsEffect
+
 import cats.effect.{ContextShift, IO}
 import net.cucumbersome.typelevelWorkshops.utils.PrintingUtils._
 
@@ -39,11 +40,11 @@ object AAIntro {
 
     h2("to future")
 
-    val startedIO =  pureValue.unsafeToFuture() // show how is it implemented
+    val startedIO = pureValue.unsafeToFuture() // show how is it implemented
 
     h1("IO and futures")
 
-    def aFutureValue(name: String): Future[Int] = Future{
+    def aFutureValue(name: String): Future[Int] = Future {
       println(s"Future is being computed, called from $name")
       54
     }(global)
@@ -70,38 +71,64 @@ object AAIntro {
     h1("composition")
 
     def pureMethod(value: Int): Int = value + 2
-    def methodWithEffect(value: String): IO[Int] = IO{ value.toInt }
-    def timeConsumingMethod[T](value: T): IO[T] = IO { Thread.sleep(1000); value }
+
+    def methodWithEffect(value: String): IO[Int] = IO {
+      value.toInt
+    }
+
+    def timeConsumingMethod[T](value: T): IO[T] = IO {
+      Thread.sleep(1000); value
+    }
 
     // notice no execution context when doing map/flatMap
     val computation1 = IO.pure("12").flatMap(methodWithEffect).map(pureMethod).flatMap(timeConsumingMethod)
     val computation2 = IO.pure("failure").flatMap(methodWithEffect).map(pureMethod).flatMap(timeConsumingMethod)
 
-    val resucedComputation = computation2.handleErrorWith{
-      case e : NumberFormatException => IO.pure(0)
+    val resucedComputation = computation2.handleErrorWith {
+      case e: NumberFormatException => IO.pure(0)
       case ex => IO.raiseError(ex)
     }
 
     res("computation1")(computation1.unsafeRunSync())
     res("rescued computation")(resucedComputation.unsafeRunSync())
+    h1("parallelism")
+
+    val longTask = IO {
+      println("starting long task")
+      Thread.sleep(500)
+      "answer"
+    }
+    val longerTask = IO {
+      println("starting longer task")
+      Thread.sleep(600)
+      12
+    }
+
+    def combineResults(str: String, int: Int): String = s"$str is $int"
+
+    implicit val contextShift = IO.contextShift(global)
+
+    val tasksInParallel: IO[String] = (longTask, longerTask).parMapN(combineResults)
+
+    res("parallel task")(tasksInParallel.unsafeRunSync())
+
+
+    h1("racing")
+
+    val fastTask = IO {
+      println("starting fast task")
+      Thread.sleep(100)
+      1
+    }
+
+    val slowerTask = IO {
+      println("starting slower task")
+      Thread.sleep(200)
+      "slower and a string"
+    }
+
+    val runTasks = IO.race(fastTask, slowerTask)
+
+    res("racing result")(runTasks.unsafeRunSync())
   }
-
-
-  h1("parallelism")
-
-  val longTask = IO{
-    println("starting long task")
-    Thread.sleep(500)
-    "answer"
-  }
-  val longerTask = IO{
-    println("starting longer task")
-    Thread.sleep(500)
-    12
-  }
-
-  def combineResults(str: String, int: Int): String = s"$str is $int"
-  implicit val contextShift = IO.contextShift(global)
-
-  val tasksInParallel: IO[String] = (longTask, longerTask).parMapN(combineResults)
 }
